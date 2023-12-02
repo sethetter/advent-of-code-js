aff0efe6346529d373e7ed37195f16033750890a
Seth Etter <sethetter@gmail.com>
2023-12-02 14:26:51 -0600

    part 2 done!

--- 2023/day02/part2.ts
+++ 2023/day02/part2.ts
@@ -8,6 +8,54 @@ if (import.meta.main) {
 }
 
 export function answer(input: string): number {
-  console.log(input);
-  return 42;
+  const games = input.split("\n").map(parseGame);
+
+  return games
+    .map((g) => minCubes(g.rounds))
+    .reduce((sum, r) => sum + power(r), 0);
+}
+
+function minCubes(rounds: Round[]): Round {
+  return {
+    r: Math.max(...rounds.map(({ r }) => r)),
+    g: Math.max(...rounds.map(({ g }) => g)),
+    b: Math.max(...rounds.map(({ b }) => b)),
+  };
+}
+
+function power({ r, g, b }: Round): number {
+  return r * g * b;
+}
+
+type Game = { id: number; rounds: Round[] };
+type Round = Record<ColorShort, number>;
+type ColorShort = "r" | "g" | "b";
+type Color = "red" | "green" | "blue";
+
+function parseGame(l: string): Game {
+  const id = parseInt(l.substring(4, l.indexOf(":")));
+  return { id, rounds: parseRounds(l) };
+}
+
+function parseRounds(l: string): Round[] {
+  return l
+    .substring(l.indexOf(": ") + 1)
+    .split("; ")
+    .map(parseRound);
+}
+
+function parseRound(rStr: string): Round {
+  const countForColor = (c: Color) =>
+    parseInt(
+      rStr
+        .trim()
+        .split(", ")
+        .find((draw) => draw.endsWith(c))
+        ?.replace(` ${c}`, "") ?? "0",
+    );
+  return {
+    r: countForColor("red"),
+    b: countForColor("blue"),
+    g: countForColor("green"),
+  };
 }
--- 2023/day02/test.ts
+++ 2023/day02/test.ts
@@ -14,6 +14,12 @@ Deno.test("part1", () => {
 });
 
 Deno.test("part2", () => {
-  const examples = ["abc", "def"].join("\n");
-  assertEquals(p2.answer(examples), 42);
+  const examples = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+  ].join("\n");
+  assertEquals(p2.answer(examples), 2286);
 });
