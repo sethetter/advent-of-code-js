97a5dc78f44cf97fc451fbafc7f5280e25600e23
Seth Etter <sethetter@gmail.com>
2023-12-04 21:20:42 -0600

    part 2 done!

--- 2023/day04/part2.ts
+++ 2023/day04/part2.ts
@@ -7,7 +7,39 @@ if (import.meta.main) {
   console.log(answer(input));
 }
 
+type card = { wNums: number[]; nums: number[]; copies: number };
+
 export function answer(input: string): number {
-  console.log(input);
-  return 42;
+  const cards = input.split("\n").map(parseCard);
+
+  for (let c = 0; c < cards.length; c++) {
+    const matches = numMatches(cards[c]);
+    for (let i = 0; i < cards[c].copies; i++) {
+      for (let cn = 1; cn <= matches; cn++) {
+        cards[c + cn].copies++;
+      }
+    }
+  }
+
+  return cards.reduce((sum, c) => sum + c.copies, 0);
+}
+
+function numMatches({ wNums, nums }: card): number {
+  return nums.filter((n) => wNums.includes(n)).length;
+}
+
+function parseCard(line: string): card {
+  const [wNums, nums] = line
+    .substring(line.indexOf(":") + 1)
+    .split(" | ")
+    .map((part) =>
+      part
+        .trim()
+        .split(" ")
+        .filter((s) => s.trim().match(/\d+/))
+        .map((n) => {
+          return parseInt(n.trim());
+        }),
+    );
+  return { wNums, nums, copies: 1 };
 }
