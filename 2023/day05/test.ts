1afb266fa736b22eab5f7a1bd880d6a2fb6371e1
Seth Etter <sethetter@gmail.com>
2023-12-05 20:40:59 -0600

    part 2, attempt 1 -- brute force, no bueno

--- 2023/day05/test.ts
+++ 2023/day05/test.ts
@@ -10,6 +10,7 @@ Deno.test("part1", () => {
     "50 98 2",
     "52 50 48",
     "",
+    // 13 + (15-0)
     "soil-to-fertilizer map:",
     "0 15 37",
     "37 52 2",
@@ -41,7 +42,42 @@ Deno.test("part1", () => {
   assertEquals(p1.answer(examples), 35);
 });
 
-// Deno.test("part2", () => {
-//   const examples = ["abc", "def"].join("\n");
-//   assertEquals(p2.answer(examples), 42);
-// });
+Deno.test("part2", () => {
+  const examples = [
+    "seeds: 79 14 55 13",
+    "",
+    "seed-to-soil map:",
+    "50 98 2",
+    "52 50 48",
+    "",
+    // 13 + (15-0)
+    "soil-to-fertilizer map:",
+    "0 15 37",
+    "37 52 2",
+    "39 0 15",
+    "",
+    "fertilizer-to-water map:",
+    "49 53 8",
+    "0 11 42",
+    "42 0 7",
+    "57 7 4",
+    "",
+    "water-to-light map:",
+    "88 18 7",
+    "18 25 70",
+    "",
+    "light-to-temperature map:",
+    "45 77 23",
+    "81 45 19",
+    "68 64 13",
+    "",
+    "temperature-to-humidity map:",
+    "0 69 1",
+    "1 0 69",
+    "",
+    "humidity-to-location map:",
+    "60 56 37",
+    "56 93 4",
+  ].join("\n");
+  assertEquals(p2.answer(examples), 46);
+});
