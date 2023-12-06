9eff706d4cd9dc1889925ce4b7b47cdb2ace6c80
Seth Etter <sethetter@gmail.com>
2023-12-05 20:23:43 -0600

    part 1

--- 2023/day05/part1.ts
+++ 2023/day05/part1.ts
@@ -1,3 +1,5 @@
+import { assert } from "https://deno.land/std@0.208.0/assert/mod.ts";
+
 if (import.meta.main) {
   const input = (
     await Deno.readFile("input").then((bytes) =>
@@ -7,7 +9,60 @@ if (import.meta.main) {
   console.log(answer(input));
 }
 
+type Range = {
+  fromStart: number;
+  toStart: number;
+  range: number;
+};
+
+type MapRanges = {
+  from: string;
+  to: string;
+  ranges: Range[];
+};
+
 export function answer(input: string): number {
-  console.log(input);
-  return 42;
+  const [seedLine, _, ...mapLines] = input.split("\n");
+  const seeds = seedLine
+    .replace("seeds: ", "")
+    .split(" ")
+    .map((x) => parseInt(x));
+
+  const mapRanges: MapRanges[] = mapLines
+    .join("\n")
+    .split("\n\n")
+    .map((mapLine) => {
+      const [fromToLine, ...rangeLines] = mapLine.split("\n");
+      const [_, from, to] = fromToLine.match(/([a-z]+)-to-([a-z]+) map/)!;
+      const ranges = rangeLines.map((rangeLine) => {
+        const [toStart, fromStart, range] = rangeLine
+          .split(" ")
+          .map((n) => parseInt(n));
+        return { fromStart, toStart, range };
+      });
+
+      return { from, to, ranges };
+    });
+
+  // start at `from: seed` to `to: location`
+  const locationNumbers = seeds.map((s) => getLocationNumber(s, mapRanges));
+
+  return Math.min(...locationNumbers);
+}
+
+function getLocationNumber(seed: number, mapRanges: MapRanges[]): number {
+  let current = "seed";
+  let currentNum = seed;
+  while (current !== "location") {
+    const mapRange = mapRanges.find(({ from }) => from === current);
+    assert(mapRange, `mapRange not found for: ${current}`);
+
+    const range = mapRange.ranges.find(
+      (r) => currentNum >= r.fromStart && currentNum <= r.fromStart + r.range,
+    );
+    if (range) currentNum = currentNum + (range.toStart - range.fromStart);
+
+    current = mapRange.to;
+  }
+  return currentNum;
 }
