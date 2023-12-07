c8d3db63099e68d453f8d28e369c6e9d129bd94c
Seth Etter <sethetter@gmail.com>
2023-12-06 20:10:41 -0600

    part 1 done!

--- 2023/day06/part1.ts
+++ 2023/day06/part1.ts
@@ -1,3 +1,5 @@
+import { assert } from "https://deno.land/std@0.208.0/assert/assert.ts";
+
 if (import.meta.main) {
   const input = (
     await Deno.readFile("input").then((bytes) =>
@@ -8,6 +10,33 @@ if (import.meta.main) {
 }
 
 export function answer(input: string): number {
-  console.log(input);
-  return 42;
+  const [raceDurations, records] = input.split("\n").map((l) => {
+    return l
+      .split(/\s+/)
+      .slice(1)
+      .map((x) => parseInt(x));
+  });
+
+  let errorMargin = 1;
+  for (let i = 0; i < raceDurations.length; i++) {
+    const [min, max] = minMaxHoldsToWin(raceDurations[i], records[i]);
+    errorMargin *= max - min + 1;
+  }
+
+  return errorMargin;
+}
+
+function minMaxHoldsToWin(
+  raceDuration: number,
+  record: number,
+): [number, number] {
+  let min = 0;
+  let max = 0;
+  for (let hold = 0; hold < raceDuration; hold++) {
+    const distance = hold * (raceDuration - hold);
+    if (min === 0 && distance > record) min = hold;
+    if (min !== 0 && distance > record) max = hold;
+    if (min !== 0 && max !== 0 && distance < record) break;
+  }
+  return [min, max];
 }
--- 2023/day06/test.ts
+++ 2023/day06/test.ts
@@ -3,8 +3,10 @@ import * as p1 from "./part1.ts";
 import * as p2 from "./part2.ts";
 
 Deno.test("part1", () => {
-  const examples = ["abc", "def"].join("\n");
-  assertEquals(p1.answer(examples), 42);
+  const examples = ["Time:      7  15   30", "Distance:  9  40  200"].join(
+    "\n",
+  );
+  assertEquals(p1.answer(examples), 288);
 });
 
 Deno.test("part2", () => {
