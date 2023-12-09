93d8063740d3f39da41cc5072063b566435d1b32
Seth Etter <sethetter@gmail.com>
2023-12-08 22:13:00 -0600

    logging

--- 2023/day08/part2.ts
+++ 2023/day08/part2.ts
@@ -19,8 +19,12 @@ export function answer(input: string): number {
   let paths = Object.keys(network).filter((n) => n.endsWith("A"));
   let stepCount = 0;
 
+  console.log(`network: ${Object.keys(network).length}`);
+  console.log(`paths: ${paths.length}`);
+
   while (!pathsFinished(paths)) {
     for (const dir of instructions.split("")) {
+      logPaths(stepCount, paths);
       paths = step(paths, dir as keyof Node);
       stepCount++;
       if (pathsFinished(paths)) break;
@@ -28,6 +32,14 @@ export function answer(input: string): number {
   }
 
   return stepCount;
+}
+
+function logPaths(stepCount: number, paths: string[]) {
+  console.log(
+    `${stepCount.toString().padStart(20, "0")} ${paths
+      .map((p) => (p.endsWith("Z") ? "x" : "."))
+      .join("")}`,
+  );
 }
 
 if (import.meta.main) {
