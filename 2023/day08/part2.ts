8ca55aec8b63f8ea4a77eccd6edf7016c3b54f8b
Seth Etter <sethetter@gmail.com>
2023-12-09 21:57:09 -0600

    ok, lcm was the missing piece

--- 2023/day08/part2.ts
+++ 2023/day08/part2.ts
@@ -1,13 +1,7 @@
 type Node = { L: string; R: string };
 type Network = Record<string, Node>;
 
-type Path = {
-  start: string;
-  current: string;
-  history: string[];
-};
-
-export function answer(input: string): number {
+export function answer(input: string) {
   const [instructionsRaw, , ...networkRaw] = input.split("\n");
 
   const network: Network = networkRaw.reduce((network, line) => {
@@ -16,60 +10,35 @@ export function answer(input: string): number {
     return { ...network, [id]: { L, R } };
   }, {});
 
-  const instructions = instructionsRaw.split("");
+  const instructions = instructionsRaw.split("") as ("L" | "R")[];
 
-  const next = (node: string, i: number): string =>
-    network[node][instructions[i] as "L" | "R"];
+  const paths = Object.keys(network).filter((n) => n.endsWith("A"));
 
-  const startNodes = Object.keys(network).filter((n) => n.endsWith("A"));
-  const endNodes = Object.keys(network).filter((n) => n.endsWith("Z"));
-
-  const paths: Path[] = startNodes.map((n) => ({
-    start: n,
-    current: n,
-    history: [],
-  }));
-
-  const stepsToZCache: Record<string, number> = {};
-
-  const stepPath = (p: Path, instIdx: number, step: number) => {
-    p.history.push(p.current);
-    p.current = next(p.current, instIdx);
-
-    if (p.current.endsWith("Z")) {
-      p.history.forEach((prev, i) => {
-        console.log(`${instIdx}.${prev}.${p.current} = ${step - i + 1}`);
-        stepsToZCache[`${instIdx}.${prev}.${p.current}`] = step - i + 1;
-      });
+  const stepsToZ = (node: string) => {
+    let step = 0;
+    while (!node.endsWith("Z")) {
+      for (const dir of instructions) {
+        node = network[node][dir];
+        step++;
+        if (node.endsWith("Z")) break;
+      }
     }
+    return step;
   };
 
-  let step = 0;
-  while (true) {
-    for (let instIdx = 0; instIdx < instructions.length; instIdx++) {
-      const stepsToZFromCurrent: number[] = [];
+  return lcm(paths.map((p) => stepsToZ(p)));
+}
 
-      paths.forEach((p) => {
-        stepPath(p, instIdx, step);
-        for (const en of endNodes) {
-          const steps = stepsToZCache[`${instIdx}.${p.current}.${en}`];
-          if (steps) stepsToZFromCurrent.push(steps);
-        }
-      });
+function lcm(nums: number[]): number {
+  const [a, b, ...rest] = nums;
+  const c = (a * b) / gcf(a, b);
+  if (rest.length === 0) return c;
+  return lcm([c, ...rest]);
+}
 
-      const pathsToZ = paths.map((p) =>
-        endNodes
-          .map((en) => stepsToZCache[`${instIdx}.${p.current}.${en}`])
-          .filter((s) => s !== undefined),
-      );
-
-      if (pathsToZ.every((p) => p.length > 0)) {
-        return pathsToZ.reduce((product, x) => (product *= x[x.length - 1]), 1);
-      }
-
-      step++;
-    }
-  }
+function gcf(a: number, b: number): number {
+  if (b === 0) return a;
+  return gcf(b, a % b);
 }
 
 if (import.meta.main) {
